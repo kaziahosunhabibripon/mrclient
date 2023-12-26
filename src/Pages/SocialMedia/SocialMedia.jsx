104c69c349360347558e2f05d7bee6d71ef31473
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-12-26 11:24:49 +0600

    Added Social Media Page

--- src/Pages/SocialMedia/SocialMedia.jsx
+++ src/Pages/SocialMedia/SocialMedia.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import {
-    FaFacebook,
-    FaInstagram,
-} from 'react-icons/fa';
 import Container from '../Shared/Container';
-import { CiFacebook } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
+import { TiSocialFacebookCircular, TiSocialGooglePlusCircular, TiSocialLinkedinCircular, TiSocialPinterestCircular, TiSocialTwitterCircular, TiSocialYoutubeCircular } from 'react-icons/ti';
+import { LiaYelp } from "react-icons/lia";
+import { BiLogoTiktok } from "react-icons/bi";
+import { SiNextdotjs } from "react-icons/si";
 
 const SocialMedia = () => {
     const { register, handleSubmit } = useForm();
@@ -18,46 +18,179 @@ const SocialMedia = () => {
     return (
         <div className='font-montserrat my-10 w-3/6 mx-auto'>
             <Container>
-                <div className='shadow-md'>
+                <div className='shadow'>
                     <div className='bg-[#1C91E4] px-5 py-4'>
                         <h4 className='text-white font-semibold'>Social Media Links</h4>
                     </div>
 
                     <div className='p-5'>
                         <form onSubmit={handleSubmit(onSubmit)}>
-                            {/* <div className='mb-4 relative flex items-center'>
-                                <FaFacebook className='text-gray-500 mr-2' />
-                                <input
-                                    type='text'
-                                    placeholder='Facebook URL'
-                                    {...register('facebook')}
-                                    className='border border-gray-300 rounded-md p-2 w-full'
-                                />
-                            </div>
-                            {/* Add similar input fields for other social media platforms */}
-                            {/* Example: Instagram */}
-                            {/* <div className='mb-4 relative flex items-center'>
-                                <FaInstagram className='text-gray-500 mr-2' />
-                                <input
-                                    type='text'
-                                    placeholder='Instagram URL'
-                                    {...register('instagram')}
-                                    className='border border-gray-300 rounded-md p-2 w-full'
-                                />
-                            </div> */}
-                            {/* Repeat similar structures for other social media platforms */}
 
                             {/* Facebook Field */}
                             <div className="mb-4 relative">
-                                <label htmlFor="password" className="block mb-1 text-sm md:text-base font-normal">Facebook</label>
-                                <div className="flex items-center border border-gray-300 p-2 pl-3">
-                                    <CiFacebook className='text-gray-500 text-2xl' />
+                                <label htmlFor="facebook" className="block mb-1 text-sm md:text-base font-normal">Facebook</label>
+                                <div className="flex items-center border border-gray-300">
+                                    <div className='border-r p-2'>
+                                        <TiSocialFacebookCircular className='text-gray-500 text-2xl' />
+                                    </div>
                                     <input
                                         type="text"
-                                        id="password"
-                                        required
+                                        id="facebook"
                                         placeholder="https://"
-                                        {...register('password', { required: 'Password is required' })}
+                                        {...register('facebook')}
+                                        className="w-full ml-2 text-sm outline-none"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Instagram Field */}
+                            <div className="mb-4 relative">
+                                <label htmlFor="instagram" className="block mb-1 text-sm md:text-base font-normal">Instagram</label>
+                                <div className="flex items-center border border-gray-300">
+                                    <div className='border-r p-2'>
+                                        <FaInstagram className='text-gray-500 text-xl' />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="instagram"
+                                        placeholder="https://"
+                                        {...register('instagram')}
+                                        className="w-full ml-2 text-sm outline-none"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Linkedin Field */}
+                            <div className="mb-4 relative">
+                                <label htmlFor="linkedin" className="block mb-1 text-sm md:text-base font-normal">Linkedin</label>
+                                <div className="flex items-center border border-gray-300">
+                                    <div className='border-r p-2'>
+                                        <TiSocialLinkedinCircular className='text-gray-500 text-2xl' />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="linkedin"
+                                        placeholder="https://"
+                                        {...register('linkedin')}
+                                        className="w-full ml-2 text-sm outline-none"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Twitter Field */}
+                            <div className="mb-4 relative">
+                                <label htmlFor="twitter" className="block mb-1 text-sm md:text-base font-normal">Twitter</label>
+                                <div className="flex items-center border border-gray-300">
+                                    <div className='border-r p-2'>
+                                        <TiSocialTwitterCircular className='text-gray-500 text-2xl' />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="twitter"
+                                        placeholder="https://"
+                                        {...register('twitter')}
+                                        className="w-full ml-2 text-sm outline-none"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Pinterest Field */}
+                            <div className="mb-4 relative">
+                                <label htmlFor="pinterest" className="block mb-1 text-sm md:text-base font-normal">Pinterest</label>
+                                <div className="flex items-center border border-gray-300">
+                                    <div className='border-r p-2'>
+                                        <TiSocialPinterestCircular className='text-gray-500 text-2xl' />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="pinterest"
+                                        placeholder="https://"
+                                        {...register('pinterest')}
+                                        className="w-full ml-2 text-sm outline-none"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Google Field */}
+                            <div className="mb-4 relative">
+                                <label htmlFor="google" className="block mb-1 text-sm md:text-base font-normal">Google</label>
+                                <div className="flex items-center border border-gray-300">
+                                    <div className='border-r p-2'>
+                                        <TiSocialGooglePlusCircular className='text-gray-500 text-2xl' />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="google"
+                                        placeholder="https://"
+                                        {...register('google')}
+                                        className="w-full ml-2 text-sm outline-none"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Youtube Field */}
+                            <div className="mb-4 relative">
+                                <label htmlFor="youtube" className="block mb-1 text-sm md:text-base font-normal">Youtube</label>
+                                <div className="flex items-center border border-gray-300">
+                                    <div className='border-r p-2'>
+                                        <TiSocialYoutubeCircular className='text-gray-500 text-2xl' />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="youtube"
+                                        placeholder="https://"
+                                        {...register('youtube')}
+                                        className="w-full ml-2 text-sm outline-none"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Yelp Field */}
+                            <div className="mb-4 relative">
+                                <label htmlFor="yelp" className="block mb-1 text-sm md:text-base font-normal">Yelp</label>
+                                <div className="flex items-center border border-gray-300">
+                                    <div className='border-r p-2'>
+                                        <LiaYelp className='text-gray-500 text-2xl' />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="yelp"
+                                        placeholder="https://"
+                                        {...register('yelp')}
+                                        className="w-full ml-2 text-sm outline-none"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Tiktok Field */}
+                            <div className="mb-4 relative">
+                                <label htmlFor="tiktok" className="block mb-1 text-sm md:text-base font-normal">Tiktok</label>
+                                <div className="flex items-center border border-gray-300">
+                                    <div className='border-r p-2'>
+                                        <BiLogoTiktok className='text-gray-500 text-xl' />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="tiktok"
+                                        placeholder="https://"
+                                        {...register('tiktok')}
+                                        className="w-full ml-2 text-sm outline-none"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Nextdoor Field */}
+                            <div className="mb-4 relative">
+                                <label htmlFor="nextdoor" className="block mb-1 text-sm md:text-base font-normal">Nextdoor</label>
+                                <div className="flex items-center border border-gray-300">
+                                    <div className='border-r p-2'>
+                                        <SiNextdotjs className='text-gray-500 text-xl' />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="nextdoor"
+                                        placeholder="https://"
+                                        {...register('nextdoor')}
                                         className="w-full ml-2 text-sm outline-none"
                                     />
                                 </div>
