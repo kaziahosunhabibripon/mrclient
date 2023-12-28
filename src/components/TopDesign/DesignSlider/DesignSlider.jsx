e2b5e58bf487f4590c6c22e94808810ff47e17a2
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-12-28 14:08:03 +0600

    added top design sliderx

--- src/components/TopDesign/DesignSlider/DesignSlider.jsx
+++ src/components/TopDesign/DesignSlider/DesignSlider.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-
-// import required modules
 import { Navigation } from 'swiper/modules';
+import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
 
 const DesignSlider = ({ portfolio }) => {
-
-    console.log(portfolio);
+    // Generate unique IDs for navigation buttons
+    const prevButtonId = `custom-prev-${Math.random().toString(36).substring(7)}`;
+    const nextButtonId = `custom-next-${Math.random().toString(36).substring(7)}`;
 
     return (
         <div>
@@ -24,21 +21,31 @@ const DesignSlider = ({ portfolio }) => {
                 pagination={{
                     clickable: true,
                 }}
-                navigation={true}
+                navigation={{
+                    prevEl: `.${prevButtonId}`,
+                    nextEl: `.${nextButtonId}`,
+                }}
                 className="mySwiper"
             >
-                {
-                    portfolio?.map((singlePortfolio, _idx) => (
-                        <SwiperSlide key={_idx}>
-                            <div className='border mt-2'>
-                                <img src={singlePortfolio?.image} alt="" />
-
-                                <p className='text-sm font-medium px-3 my-2'>{singlePortfolio?.title}</p>
-                            </div>
-                        </SwiperSlide>
-                    ))
-                }
+                {portfolio?.map((singlePortfolio, _idx) => (
+                    <SwiperSlide key={_idx}>
+                        <div className='border mt-2'>
+                            <img src={singlePortfolio?.image} alt="" />
+                            <p className='text-sm font-medium px-3 my-2'>{singlePortfolio?.title}</p>
+                        </div>
+                    </SwiperSlide>
+                ))}
             </Swiper>
+
+            <div className='flex justify-between items-center'>
+                <button className={`custom-swiper-button bg-red-500 rounded-full text-webPrimary ${prevButtonId}`}>
+                    <MdNavigateBefore className='text-2xl' />
+                </button>
+
+                <button className={`custom-swiper-button bg-red-500 rounded-full text-webPrimary ${nextButtonId}`}>
+                    <MdNavigateNext className='text-2xl' />
+                </button>
+            </div>
         </div>
     );
 };
